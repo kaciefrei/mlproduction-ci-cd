efdc45514777f0c0c751c4da8c21cb462164bbbc
kaciefrei <kaci.bouaouli@efrei.net>
2024-11-28 11:44:28 +0100

    test

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URL || "YOUR_MONGO_DB_CONNECTION_STRING";
+const mongoURI = process.env.MONGO_URL || "mongodb+srv://kacibouaouli:1kqEL02rE6inH2XX@mlforproduction.pkcv7.mongodb.net";
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -18,5 +18,5 @@ app.use('/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Servers is running on port ${PORT}`);
 });
